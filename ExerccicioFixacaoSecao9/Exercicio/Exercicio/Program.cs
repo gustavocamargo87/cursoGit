bae8cbb49b5137107477a0152c4949ed1c78bd56
Gustavo Camargo <gustavojcamargo@hotmail.com>
2021-10-31 13:42:43 -0300

    exercicio fixacao secao 9

---
+++ ExerccicioFixacaoSecao9/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using Exercicio.Entities;
+using Exercicio.Entities.Enums;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string cliName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(cliName, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) 
+            {
+                Console.WriteLine("Enter #" + i + " item data:");
+                Console.Write("Product name: ");
+                string proName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product prod = new Product(proName, price);
+
+                Console.Write("Quantity: ");
+                int qtd = int.Parse(Console.ReadLine());
+                OrderItem item = new OrderItem(qtd, price, prod);
+
+                order.AddItem(item);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+        }
+    }
+}
